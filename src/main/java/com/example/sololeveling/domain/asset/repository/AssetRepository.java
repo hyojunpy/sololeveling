0853122aa9ffaedb0f17bcc269267a13a57f5a15
Hyojun Pyeon <vusgywns5946@gmail.com>
2025-09-24 00:27:57 +0900

    Feat : Asset crud 완료

--- src/main/java/com/example/sololeveling/domain/asset/repository/AssetRepository.java
+++ src/main/java/com/example/sololeveling/domain/asset/repository/AssetRepository.java
@@ -1,4 +1,13 @@
 package com.example.sololeveling.domain.asset.repository;
 
-public interface AssetRepository {
+import com.example.sololeveling.domain.asset.entity.Asset;
+import com.example.sololeveling.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssetRepository extends JpaRepository<Asset, Long> {
+    Page<Asset> findAllByUser(User user, Pageable pageable);
 }
